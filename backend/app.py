c45ff769078f90fc507149285b63e5045eae385c
Puneeth K G <puneethkg22@gmail.com>
2026-07-24 10:37:18 +0000

    Day-3 Connect Flask backend to SQLite database

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,9 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import sqlite3
 
 app = Flask(__name__)
 CORS(app)
-
-employees = [
-    {
-        "id": 1,
-        "name": "Alice",
-        "department": "HR"
-    },
-    {
-        "id": 2,
-        "name": "Bob",
-        "department": "Engineering"
-    },
-    {
-        "id": 3,
-        "name": "Charlie",
-        "department": "Finance"
-    }
-]
 
 @app.route("/")
 def home():
@@ -32,7 +15,20 @@ def home():
 
 @app.route("/employees")
 def get_employees():
-    return jsonify(employees)
+
+    conn = sqlite3.connect("../database/employees.db")
+
+    conn.row_factory = sqlite3.Row
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM employees")
+
+    employees = cursor.fetchall()
+
+    conn.close()
+
+    return jsonify([dict(employee) for employee in employees])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
